b3d686ec5c94b63fd598e922cb78caaa076e2751
Alan Smith <a.bax.smith@gmail.com>
2015-08-18 06:24:26 -0600

    adds search param endpoint to api

--- api/v1/pizzerias.rb
+++ api/v1/pizzerias.rb
@@ -16,11 +16,27 @@ namespace '/api' do
       geojson_data['features'][id].to_json
     end
 
+    get '/properties/search' do
+      content_type :json
+      @query = params.keys.first
+      @pizzerias = geojson_data['features']
+
+      valid_query? ? return_search_results.to_json : []
+    end
+
+
   end
 end
 
 private
   def geojson_data
-    file = File.read('pizza_map.geojson')
-    JSON.parse(file)
+    JSON.parse(File.read('pizza_map.geojson'))
+  end
+
+  def valid_query?
+    @pizzerias.any?{|pizzeria| pizzeria['properties'].has_key?(@query)}
+  end
+  
+  def return_search_results
+    @pizzerias.select{ |pizzeria| pizzeria['properties'][@query].downcase == params[@query].downcase}
   end
