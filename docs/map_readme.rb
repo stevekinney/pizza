ac967707864b8cd7d3d83d7134540e62555eeb57
Eli Sadoff <esadoff@ardiangroup.com>
2017-07-28 21:32:09 -0400

    Fix string as key error

--- docs/map_readme.rb
+++ docs/map_readme.rb
@@ -15,47 +15,47 @@ pizzerias = pizza.css('li')
 
 pizzeria_hash =
   {
-    'type' => 'FeatureCollection',
-    'crs' => {
-      'type' => 'name',
-      'properties' => {
-        'name' => 'urn:ogc:def:crs:OGC:1.3:CRS84'
+    type: 'FeatureCollection',
+    crs: {
+      type: 'name',
+      properties: {
+        name: 'urn:ogc:def:crs:OGC:1.3:CRS84'
       }
     },
-    'features' => [
+    features: [
     ]
   }
 
 pizzerias.each do |pizzeria|
   pizzeria.css('[href="#address"]').each do |location|
     pizzeria_obj = {
-      'type' => 'Feature',
-      'properties' => {
-        'city' => nil,
-        'pizzeria' => pizzeria.first_element_child.text.chomp,
-        'website' => pizzeria.child['href'].chomp,
-        'address' => location.text.chomp,
-        'marker-size' => 'medium',
-        'marker-color' => 'ffff00',
-        'marker-symbol' => 'restaurant'
+      type: 'Feature',
+      properties: {
+        city: nil,
+        pizzeria: pizzeria.first_element_child.text.chomp,
+        website: pizzeria.child['href'].chomp,
+        address: location.text.chomp,
+        'marker-size': 'medium',
+        'marker-color': 'ffff00',
+        'marker-symbol': 'restaurant'
       },
-      'geometry' => {
-        'type' => 'Point',
-        'coordinates' => nil
+      geometry: {
+        type: 'Point',
+        coordinates: nil
       }
     }
-    pizzeria_hash['features'] << pizzeria_obj
+    pizzeria_hash[:features] << pizzeria_obj
   end
 end
 
-pizzeria_hash['features'].each do |x|
+pizzeria_hash[:features].each do |x|
   cities.each do |city|
     pizzeria1 = city.next_sibling.next_sibling.text.downcase.delete("\n").gsub(/[^a-z]/, '')
-    pizzeria2 = x['properties']['pizzeria'].downcase.delete("\n").gsub(/[^a-z]/, '')
+    pizzeria2 = x[:properties][:pizzeria].downcase.delete("\n").gsub(/[^a-z]/, '')
     next unless pizzeria1.include?(pizzeria2)
-    x['properties']['city'] = city.text.chomp
-    geo_result = Geocoder.search(x['properties']['address'] + ', ' + city.text)
-    x['geometry']['coordinates'] = [geo_result.first.longitude, geo_result.first.latitude]
+    x[:properties][:city] = city.text.chomp
+    geo_result = Geocoder.search(x[:properties][:address] + ', ' + city.text)
+    x[:geometry][:coordinates] = [geo_result.first.longitude, geo_result.first.latitude]
     sleep 0.2
   end
 end
